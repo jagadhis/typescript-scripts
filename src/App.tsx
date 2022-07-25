7be953baa241b566bd915981982efaf383c998d4
jagadhis <jwaran78@gmail.com>
2022-07-25 11:21:19 +0530

    Generics

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,15 @@ import './App.css';
 
 function App() {
 
-  
+  function loggingIdentity<Type>(arg: Type[]): Type[] {
+    console.log(arg.length);
+    return arg;
+  }
+  console.log(loggingIdentity([1,2,3,4,5]));
+
   return (
     <div className="App">
+   
    
     </div>
   );
